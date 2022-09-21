77f769e85a0f4be60929fabf8a3a44fe22cc4f8a
imryche <imryche13@gmail.com>
2022-09-21 20:37:56 +0300

    script to make symlinks

---
+++ link.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
+ln -s ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/dotfiles/.config/kitty/nord.conf ~/.config/kitty/nord.conf
