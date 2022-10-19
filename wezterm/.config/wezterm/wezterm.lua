129cdcffbdd06a043303d1424f66c607ef6e5146
imryche <imryche13@gmail.com>
2022-10-19 10:51:58 +0300

    update configs

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+return {
+  enable_tab_bar = false,
+  font = wezterm.font('JetBrains Mono', { weight = 'Medium' }),
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+  font_size = 11.0,
+  color_scheme = 'nord',
+}
