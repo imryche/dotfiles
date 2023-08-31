be9896eacacf6f0642f6182404698efc4fc559e0
ryche <imryche13@gmail.com>
2023-08-31 10:10:30 +0200

    update find file mappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -245,8 +245,8 @@ local builtin = require 'telescope.builtin'
 
 vim.keymap.set('n', '<leader>?', builtin.oldfiles)
 vim.keymap.set('n', '<leader><space>', builtin.buffers)
-vim.keymap.set('n', '<leader>p', builtin.find_files)
-vim.keymap.set('n', '<leader>fp', builtin.git_files)
+vim.keymap.set('n', '<leader>p', builtin.git_files)
+vim.keymap.set('n', '<leader>P', builtin.find_files)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags)
 vim.keymap.set('n', '<leader>fw', builtin.grep_string)
 vim.keymap.set('n', '<leader>fs', builtin.live_grep)
