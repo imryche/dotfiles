acefd19261e6efaa113fef32c26b6e5c41b7e3e1
ryche <imryche13@gmail.com>
2023-10-24 09:43:09 +0200

    keymap to resume telescope

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,3 @@
-local telescope_theme = 'ivy'
 require('telescope').setup {
   defaults = {
     mappings = {
@@ -7,13 +6,6 @@ require('telescope').setup {
         ['<C-d>'] = false,
       },
     },
-  },
-  pickers = {
-    buffers = { theme = telescope_theme },
-    git_files = { theme = telescope_theme },
-    find_files = { theme = telescope_theme },
-    grep_string = { theme = telescope_theme },
-    live_grep = { theme = telescope_theme },
   },
 }
 
@@ -27,3 +19,4 @@ vim.keymap.set('n', '<C-p>', builtin.git_files)
 vim.keymap.set('n', '<C-M-p>', builtin.find_files)
 vim.keymap.set('n', '<leader>?', builtin.grep_string)
 vim.keymap.set('n', '<leader>/', builtin.live_grep)
+vim.keymap.set('n', '<leader>.', builtin.resume)
