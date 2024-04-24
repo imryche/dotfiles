f046cb3c6701c49173cdb61cae7ba001fcb48bba
ryche <imryche13@gmail.com>
2024-04-24 11:05:29 +0200

    change wezterm font size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,6 +5,6 @@ return {
   enable_tab_bar = false,
   font = wezterm.font("JetBrains Mono", { weight = "Medium" }),
   harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-  font_size = 11.0,
+  font_size = 10.0,
   color_scheme = "iceberg-dark",
 }
