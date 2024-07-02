781510c0d4ac2e5e0dc332b5a038af27051ef8b3
ryche <imryche13@gmail.com>
2024-07-02 16:49:22 +0200

    no window decorations for wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 return {
   front_end = "WebGpu",
   enable_tab_bar = false,
+  window_decorations = "RESIZE",
   font = wezterm.font("JetBrains Mono", { weight = "Medium" }),
   harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
   font_size = 10.0,
