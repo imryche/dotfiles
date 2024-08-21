c171b0682ca0d61aa7f2c3dfa250c5d0d4dc47f8
ryche <imryche13@gmail.com>
2024-08-21 16:23:55 +0200

    bidirectional leaps

---
+++ nvim/.config/nvim/after/plugin/leap.lua
@@ -0,0 +1,14 @@
+vim.keymap.set('n', 's', '<Plug>(leap)')
+vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+vim.keymap.set({ 'x', 'o' }, 's', '<Plug>(leap-forward)')
+vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+
+-- Define equivalence classes for brackets and quotes, in addition to
+-- the default whitespace group.
+require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+
+-- Use the traversal keys to repeat the previous motion without explicitly
+-- invoking Leap.
+require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+
+vim.api.nvim_set_hl(0, 'LeapLabel', { link = 'Search' })
