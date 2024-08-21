4f8381d9ca7c427dc850ec6d70f7b04bc494a52c
ryche <imryche13@gmail.com>
2024-08-21 16:24:36 +0200

    update treesitter config

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,8 +1,9 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'sql', 'typescript', 'vimdoc' },
-  autotag = { enable = true },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'sql', 'typescript', 'vimdoc', 'html' },
   highlight = { enable = true },
   indent = { enable = true },
 }
 
 vim.cmd [[highlight! link TreesitterContext SignColumn]]
+
+require('nvim-ts-autotag').setup {}
