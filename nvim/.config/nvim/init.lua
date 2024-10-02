860f915badb11b71cb7ed974a173b3ed084755d0
ryche <imryche13@gmail.com>
2024-10-02 20:50:41 +0200

    Revert 1 commits
    c59dd60 'update autocompletion keybindings'

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -383,14 +383,16 @@ require('lazy').setup {
           end,
         },
         mapping = cmp.mapping.preset.insert {
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-l>'] = cmp.mapping.complete {},
+          ['<C-Space>'] = cmp.mapping.complete {},
           ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
           },
-          ['<C-n>'] = cmp.mapping(function(fallback)
+          ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
             elseif luasnip.expand_or_locally_jumpable() then
@@ -399,7 +401,7 @@ require('lazy').setup {
               fallback()
             end
           end, { 'i', 's' }),
-          ['<C-p>'] = cmp.mapping(function(fallback)
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
             elseif luasnip.locally_jumpable(-1) then
