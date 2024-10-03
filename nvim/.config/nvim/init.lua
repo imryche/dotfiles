e6bc4e7390cced87354870963415362c5e9f9a3f
ryche <imryche13@gmail.com>
2024-10-03 19:52:15 +0200

    highlight when yanking

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -97,6 +97,15 @@ vim.api.nvim_create_autocmd('CursorMoved', {
   end,
 })
 
+-- Highlight when yanking
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
