72bb9287ddb149d83491e1bf8b16e4230c625175
ryche <imryche13@gmail.com>
2024-10-03 19:52:44 +0200

    trigger signature help in insert mode

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -235,6 +235,7 @@ require('lazy').setup {
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
           map('K', vim.lsp.buf.hover, 'Symbol info')
+          map('<C-s>', vim.lsp.buf.signature_help, 'Signature help', 'i')
           map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
           map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
           map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
