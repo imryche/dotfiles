bd24eeb7e3956d813c9d3c557bffae71794edb4f
ryche <imryche13@gmail.com>
2024-11-16 00:09:47 +0100

    remove iceberg theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -117,23 +117,12 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
   -- Colorscheme
-  -- {
-  --   'oahlen/iceberg.nvim',
-  --   priority = 1000,
-  --   init = function()
-  --     vim.cmd.colorscheme 'iceberg'
-  --   end,
-  -- },
   {
     'zenbones-theme/zenbones.nvim',
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
     dependencies = 'rktjmp/lush.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.g.zenbones_darken_comments = 45
       vim.cmd.colorscheme 'zenbones'
     end,
   },
