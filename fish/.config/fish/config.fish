53b6c18089274a06397e133e0c41e5769cb334b7
Dima <imryche13@gmail.com>
2025-01-14 14:16:03 +0100

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,53 +2,19 @@
 alias va="source .venv/bin/activate.fish"
 alias vd="deactivate.fish"
 
-set -x PATH $HOME/.local/bin $PATH
-set -x PATH $PATH /opt/nvim
+set -gx PATH $HOME/.local/bin $PATH
 
 # default editor
-set -x EDITOR nvim
+set -gx EDITOR nvim
 
 # python
-set -x PYTHONBREAKPOINT ipdb.set_trace
-set -x PYTHONDONTWRITEBYTECODE 1
-
-# lua
-set -x PATH $HOME/.config/lsp/lua-language-server/bin $PATH
+set -gx PYTHONBREAKPOINT ipdb.set_trace
+set -gx PYTHONDONTWRITEBYTECODE 1
 
 # golang
-set -x PATH $PATH /usr/local/go/bin
-set -x GOPATH $HOME/go
-set -x PATH $PATH $GOPATH/bin
-
-# nvm
-set --universal nvm_default_version lts
-
-# deno
-set -x DENO_INSTALL $HOME/.deno
-set -x PATH $DENO_INSTALL/bin $PATH
-
-# rust
-set -x PATH $HOME/.cargo/bin $PATH
-
-# starship
-starship init fish | source
+set -gx PATH $PATH /usr/local/go/bin
+set -gx GOPATH $HOME/go
+set -gx PATH $PATH $GOPATH/bin
 
 # zellij
 eval (zellij setup --generate-auto-start fish | string collect)
-
-# zoxide
-zoxide init fish | source
-
-# fzf
-fzf --fish | source
-
-# direnv
-direnv hook fish | source
-
-# pyenv
-set -x PYENV_ROOT $HOME/.pyenv
-if test -d $PYENV_ROOT/bin
-    set -x PATH $PYENV_ROOT/bin $PATH
-end
-pyenv init --path | source
-pyenv init - | source
