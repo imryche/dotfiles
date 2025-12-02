af55577cfc7650cce2fbda48a1a875a7dd09a8e7
imryche <imryche13@gmail.com>
2025-12-02 10:08:49 +0100

    disable hot corners

--- setup.sh
+++ setup.sh
@@ -35,6 +35,7 @@ configure_gnome() {
     gsettings set org.gnome.desktop.interface scaling-factor 2
     gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 20
     gsettings set org.gnome.desktop.peripherals.keyboard delay 200
+    gsettings set org.gnome.desktop.interface enable-hot-corners false
     echo "GNOME configured"
 }
 
