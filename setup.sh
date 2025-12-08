2393e99a5b414593ea63d40c3d01ff342c67e516
Dima <imryche13@gmail.com>
2025-12-08 22:19:46 +0100

    install fd

--- setup.sh
+++ setup.sh
@@ -159,7 +159,7 @@ configure_git() { stow_dotfiles gitconfig; }
 # CLI tools (zoxide, ripgrep, fzf, gh)
 install_cli_tools() {
     gum style --foreground 33 "Installing CLI tools..."
-    install_dnf zoxide ripgrep fzf gh htop btop nvtop
+    install_dnf zoxide ripgrep fd-find fzf gh htop btop nvtop
 }
 
 # mise runtime manager
